c61958b52fe2370d13ba1ce39261bb683d54a36c
Babul Shree Chaudhary <talk2babul77@gmail.com>
2022-06-21 09:50:53 +0545

    Added simple interest calculator project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import { Container } from '@mui/material';
 
-function App() {
+const App = () => {
+  const [p, setP] = useState(0)
+  const [t, setT] = useState(0)
+  const [r, setR] = useState(0)
+  const [si, setSI] = useState(0)
+
+  const simpleInterest = () => {
+    setSI(p*t*r/100)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Simple Interest
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <br />
+      <Container>
+
+        <TextField onChange={(event) =>setP(event.target.value)} id="outlined-basic" label="Enter P" variant="outlined" /><br /><br />
+        <TextField onChange={(event) =>setT(event.target.value)} id="outlined-basic" label="Enter T" variant="outlined" /><br /><br />
+        <TextField onChange={(event) =>setR(event.target.value)} id="outlined-basic" label="Enter R" variant="outlined" /><br /><br />
+        <Button onClick={() => simpleInterest()} variant="contained">Calculate SI</Button><br /><br />
+        <Typography variant="h6" gutterBottom component="div">
+        Simple Interest is: {si}
+        </Typography>
+      </Container>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
